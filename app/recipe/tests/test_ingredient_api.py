698fd9692bae935b9ac2f3fd0526a55e717cad3b
coderiddles <godspoweropara37@gmail.com>
2020-02-28 18:53:55 +0100

    implement feature for filtering tags and ingredients

--- app/recipe/tests/test_ingredient_api.py
+++ app/recipe/tests/test_ingredient_api.py
@@ -4,7 +4,7 @@ from django.test import TestCase
 
 from rest_framework import status
 from rest_framework.test import APIClient
-from core.models import Ingredient
+from core.models import Ingredient, Recipe
 from recipe.serializers import IngredientSerializer
 
 
@@ -79,3 +79,21 @@ class PrivateIngredientApiTests(TestCase):
         res = self.client.post(INGREDIENTS_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_retrieve_ingredients_assigned_to_recipes(self):
+        """ Test filtering ingredients by those assigned to recipe """
+        ingredient1 = Ingredient.objects.create(user=self.user, name='Curry')
+        ingredient2 = Ingredient.objects.create(user=self.user, name='Cabbage')
+        recipe = Recipe.objects.create(
+            title='Coriander eggs and bread',
+            price=12.00,
+            time_minutes=10,
+            user=self.user
+        )
+        recipe.ingredients.add(ingredient1)
+        res = self.client.get(INGREDIENTS_URL, {'assigned_only': 1})
+        serializer1 = IngredientSerializer(ingredient1)
+        serializer2 = IngredientSerializer(ingredient2)
+
+        self.assertIn(serializer1.data, res.data)
+        self.assertNotIn(serializer2.data, res.data)
